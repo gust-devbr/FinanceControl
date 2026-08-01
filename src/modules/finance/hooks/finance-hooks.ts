3f7e8af1518b780917ab9e000a58988d9e139c1b
Gustavo <calacogustavo@gmail.com>
2026-07-31 22:48:07 -0300

    refactor: separação do formulário da página

--- src/modules/finance/hooks/finance-hooks.ts
+++ src/modules/finance/hooks/finance-hooks.ts
@@ -1,4 +1,4 @@
-import { useQuery, useMutation } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { useAppMutation } from "@/hooks/use-app-mutation";
 
 import { QUERY_KEYS } from "@/lib/query-keys";
